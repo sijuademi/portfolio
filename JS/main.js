4358488269efd9dbb31a1754ce19368db280dfb5
Khalil Ibrahim <sijuadebayo@gmail.com>
2022-01-14 03:34:33 +0100

    fixed fixed navbar issue

--- JS/main.js
+++ JS/main.js
@@ -43,16 +43,16 @@ $(document).ready(function () {
 
     // sticky navigation menu
 
-    let nav_offset_top = $('.header_area').height() + 50;
+    let nav_offset_top = $('.header-area').height() + 50;
 
     function navbarFixed() {
-        if ($('.header_area').length) {
+        if ($('.header-area').length) {
             $(window).scroll(function () {
                 let scroll = $(window).scrollTop();
                 if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
+                    $('.header-area .main-menu').addClass('navbar_fixed');
                 } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                    $('.header-area .main-menu').removeClass('navbar_fixed');
                 }
             })
         }
